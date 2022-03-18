308b1814e086a56c227254f333ccf8c1cf687d86
Anton Poot <antonpoot41@gmail.com>
2022-03-18 14:13:38 -0600

    ciclo while

---
+++ repetitiva_mientra_1.py
@@ -0,0 +1,16 @@
+'''
+Script en python que muestre los números del 0 al 13 utilizando
+la estructura repetitiva "mientras"
+'''
+
+print('Programa que muestra los numeros del 0 al 13')
+#contador
+numero = 0
+
+while numero <= 13:
+    print(f'contador : {numero}')
+    numero = numero + 1
+else:
+     print('He terminado de contar')
+
+print('Nos vemos')
